088610f646e32e9378eb66d57cd6d0d6ed8347a6
Shivansh Ghelani <120641381+ShivanshGhelani@users.noreply.github.com>
2025-02-21 21:50:42 +0530

    Update ocr_api.py

--- ocr_api.py
+++ ocr_api.py
@@ -83,4 +83,4 @@ async def extract_text(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=10000, reload=True)
